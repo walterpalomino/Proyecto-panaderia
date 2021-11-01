bf62a3ed14b9eefdb95fe7d9b047efb86c7b63ce
walter palomino <jimmypalomino93@gmail.com>
2021-11-01 01:15:15 -0300

    Se creo una exception controlada para le ingreso de datos

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/exception/InvalidDataException.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/exception/InvalidDataException.java
@@ -1,2 +1,22 @@
-package com.microservicio.app.panaderia.exception;public class InvalidDataException {
+package com.microservicio.app.panaderia.exception;
+
+import org.springframework.validation.BindingResult;
+
+public class InvalidDataException extends RuntimeException{
+
+    private final transient BindingResult result;
+
+    public InvalidDataException(BindingResult result) {
+        super();
+        this.result = result;
+    }
+
+    public InvalidDataException(String message, BindingResult result) {
+        super(message);
+        this.result = result;
+    }
+
+    public BindingResult getResult() {
+        return result;
+    }
 }
