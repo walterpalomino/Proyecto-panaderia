288e953a1cb87043adaeb1e6636c6a19331b91c0
walter palomino <jimmypalomino93@gmail.com>
2021-11-19 01:19:36 -0300

    Se limpio los import y se elimino el constructor porque lo construye lombok

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/entity/Pedido.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/entity/Pedido.java
@@ -1,9 +1,5 @@
 package com.microservicio.app.panaderia.entity;
 
-import java.io.Serializable;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -11,21 +7,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Data
 @NoArgsConstructor @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "pedidos")
 public class Pedido {
 
@@ -41,15 +34,4 @@ public class Pedido {
 				
 		this.cliente = cliente;
 	}
-
-	public Pedido(Long id, Cliente cliente) {
-	
-		this.id = id;
-		this.cliente = cliente;
-	}
-	
-	
-	
-	
-
 }
