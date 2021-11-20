7696a7529e0a31fe03359f4272c3a3a2efc3a7cf
walter palomino <jimmypalomino93@gmail.com>
2021-11-20 18:59:31 -0300

    Se cambio el tipo de parametro de entrada del metodo

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/ClienteServicio.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/ClienteServicio.java
@@ -2,12 +2,13 @@ package com.microservicio.app.panaderia.servicio;
 
 import java.util.List;
 
+import com.microservicio.app.panaderia.dto.ClienteCrearDto;
 import com.microservicio.app.panaderia.dto.ClienteDto;
 import com.microservicio.app.panaderia.entity.Cliente;
 
 public interface ClienteServicio<T> {
 	
 	public List<ClienteDto> buscarClientes();
-	public T createCliente(Cliente cliente);
+	public T createCliente(ClienteCrearDto clienteCrearDto);
 
 }
