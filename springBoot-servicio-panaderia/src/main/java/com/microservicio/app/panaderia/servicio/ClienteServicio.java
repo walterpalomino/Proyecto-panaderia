374b6892a0993351437823940bb1ae61f1892149
walter palomino <jimmypalomino93@gmail.com>
2021-11-21 12:20:29 -0300

    Se cambio tipo de dato que recibe por parametro el metodo que actualiza los datos del cliente

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/ClienteServicio.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/ClienteServicio.java
@@ -11,7 +11,7 @@ public interface ClienteServicio {
 	public List<ClienteDto> buscarClientes();
 	public ClienteDto createCliente(ClienteCrearDto clienteCrearDto);
 	public ClienteDto buscarClienteId(long id);
-	public ClienteDto actualizarCliente(long id, ClienteDto clienteDto);
+	public ClienteDto actualizarCliente(long id, ClienteCrearDto clienteDto);
 	public void eliminarCliente();
 
 }
