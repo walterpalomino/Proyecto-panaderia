185d9d5b3d79baa5a66b6181b0c6d7196e67af46
walter palomino <jimmypalomino93@gmail.com>
2021-11-21 12:03:08 -0300

    Se cambio el tipo de dato de los metodos del service

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/ClienteServicio.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/ClienteServicio.java
@@ -6,9 +6,12 @@ import com.microservicio.app.panaderia.dto.ClienteCrearDto;
 import com.microservicio.app.panaderia.dto.ClienteDto;
 import com.microservicio.app.panaderia.entity.Cliente;
 
-public interface ClienteServicio<T> {
+public interface ClienteServicio {
 	
 	public List<ClienteDto> buscarClientes();
-	public T createCliente(ClienteCrearDto clienteCrearDto);
+	public ClienteDto createCliente(ClienteCrearDto clienteCrearDto);
+	public ClienteDto buscarClienteId(long id);
+	public ClienteDto actualizarCliente(long id, ClienteDto clienteDto);
+	public void eliminarCliente();
 
 }
