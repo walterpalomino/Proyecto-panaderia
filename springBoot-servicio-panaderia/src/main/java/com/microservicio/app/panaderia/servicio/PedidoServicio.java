b0cd52cdee1b9484996b0b5439ce51e1af8a15ca
walter palomino <jimmypalomino93@gmail.com>
2021-11-24 00:02:26 -0300

    Se cambiaron los tipos de datos de los metodos y agregaron dos metodos el editar y eliminar

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/PedidoServicio.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/PedidoServicio.java
@@ -2,12 +2,16 @@ package com.microservicio.app.panaderia.servicio;
 
 import java.util.List;
 
+import com.microservicio.app.panaderia.dto.PedidoCrearDto;
+import com.microservicio.app.panaderia.dto.PedidoDto;
 import com.microservicio.app.panaderia.entity.Pedido;
 
 public interface PedidoServicio {
 
-	public List<Pedido> findAll();
-	public Pedido addPedido(Pedido p);
-	public Pedido findById(Long id) throws Exception;
+	public List<PedidoDto> findAll();
+	public PedidoDto addPedido(PedidoCrearDto pedidoCrearDto);
+	public PedidoDto findById(Long id);
+	public PedidoDto updatePedido(long id, PedidoCrearDto pedidoCrearDto);
+	public void deletePedido(long id);
 	
 }
