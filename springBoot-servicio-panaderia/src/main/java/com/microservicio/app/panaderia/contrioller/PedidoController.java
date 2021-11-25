7ad5a96f30ab9da180c07aadd257eaa3f405fc87
walter palomino <jimmypalomino93@gmail.com>
2021-11-25 01:10:32 -0300

    Se agrego el controlador que actualiza un pedido

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/contrioller/PedidoController.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/contrioller/PedidoController.java
@@ -9,12 +9,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.microservicio.app.panaderia.entity.Pedido;
 import com.microservicio.app.panaderia.servicio.PedidoServicio;
@@ -25,40 +20,33 @@ public class PedidoController {
 	private static final Log log = LogFactory.getLog(PedidoController.class);
 
 	@Autowired
-	private PedidoServicio servicio;
+	private PedidoServicio pedidoServicio;
 
 	@GetMapping("/listadoPedido")
 	public ResponseEntity<List<PedidoDto>> listado() {
-	//	try {
 
-			return ResponseEntity.ok(servicio.findAll());
-
-//		} catch (Exception e) {
-		//	log.error(e);
-			//return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-	//	}
-
+			return ResponseEntity.ok(pedidoServicio.findAll());
 	}
 	
 	@GetMapping("/buscar/{id}")
 	public ResponseEntity<PedidoDto> buscarPedido(@PathVariable Long id) {
 
-			return ResponseEntity.ok(servicio.findById(id));
+			return ResponseEntity.ok(pedidoServicio.findById(id));
 	}
-/*
+
 	@PostMapping("/guardarPedido")
-	public ResponseEntity<Pedido> addPedido(@RequestBody PedidoCrearDto p) {
-		try {
-			
-			log.error(p.getCliente().getId());
-			return ResponseEntity.status(HttpStatus.CREATED).body(servicio.addPedido(p));
-		} catch (Exception e) {
+	public ResponseEntity<PedidoDto> addPedido(@RequestBody PedidoCrearDto pedidoCrearDto) {
 
-			log.error(e);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
+			log.info(pedidoCrearDto.toString());
+			return ResponseEntity.status(HttpStatus.CREATED).body(pedidoServicio.addPedido(pedidoCrearDto));
+	}
+
+	@PutMapping("/actualizar-pedido/{id}")
+	public ResponseEntity<PedidoDto> actualizarPedido(@PathVariable long id, @RequestBody PedidoCrearDto pedidoCrearDto){
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(pedidoServicio.updatePedido(id, pedidoCrearDto));
 	}
 
 
- */
+
 }
