ab065a9be953bcc08626841882227d5a40e1220b
walter palomino <jimmypalomino93@gmail.com>
2021-11-27 22:06:12 -0300

    Se agrego el service producto

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/ProductoService.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/ProductoService.java
@@ -1,2 +1,17 @@
-package com.microservicio.app.panaderia.servicio;public interface ProductoService {
+package com.microservicio.app.panaderia.servicio;
+
+import com.microservicio.app.panaderia.dto.ClienteCrearDto;
+import com.microservicio.app.panaderia.dto.ClienteDto;
+import com.microservicio.app.panaderia.dto.ProductoCrearDto;
+import com.microservicio.app.panaderia.dto.ProductoDto;
+
+import java.util.List;
+
+public interface ProductoService {
+
+    public List<ProductoDto> buscarClientes();
+    public ProductoDto createCliente(ProductoCrearDto productoCrearDto);
+    public ProductoDto buscarClienteId(long id);
+    public ProductoDto actualizarCliente(long id, ProductoCrearDto productoCrearDto);
+    public void eliminarCliente(long id);
 }
