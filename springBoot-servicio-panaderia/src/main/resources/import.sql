b2fca9db919cd732782208e4295c212bacd9327b
walter palomino <jimmypalomino93@gmail.com>
2021-11-27 21:33:41 -0300

    Se agrego campos a la tabla producto

--- springBoot-servicio-panaderia/src/main/resources/import.sql
+++ springBoot-servicio-panaderia/src/main/resources/import.sql
@@ -2,17 +2,17 @@ insert into clientes(nombre) values('pepe');
 insert into clientes(nombre) values('juan');
 insert into clientes(nombre) values('pepito');
 
-insert into producto(nombre) values('criollo comun');
-insert into producto(nombre) values('pan tira');
-insert into producto(nombre) values('pan bollito');
-insert into producto(nombre) values('criollo hojaldre');
-insert into producto(nombre) values('chipaca');
+insert into producto(nombre, precio, stock_minimo, stock_maximo, stock_actual) values('criollo comun', 15.20, 5, 30, 20);
+insert into producto(nombre, precio, stock_minimo, stock_maximo, stock_actual) values('pan tira', 15.20, 5, 30, 20);
+insert into producto(nombre, precio, stock_minimo, stock_maximo, stock_actual) values('pan bollito', 15.20, 5, 30, 20);
+insert into producto(nombre, precio, stock_minimo, stock_maximo, stock_actual) values('criollo hojaldre', 15.20, 5, 30, 20);
+insert into producto(nombre, precio, stock_minimo, stock_maximo, stock_actual) values('chipaca', 15.20, 5, 30, 20);
 
-insert into pedidos(cliente) values(1);
-insert into pedidos(cliente) values(2);
-insert into pedidos(cliente) values(3);
-insert into pedidos(cliente) values(1);
-insert into pedidos(cliente) values(2);
+insert into pedidos(cliente, importe_total, fecha) values(1, 15.30, '24/11/2021');
+insert into pedidos(cliente, importe_total, fecha) values(2, 15.30, '24/11/2021');
+insert into pedidos(cliente, importe_total, fecha) values(3, 15.30, '24/11/2021');
+insert into pedidos(cliente, importe_total, fecha) values(1, 15.30, '24/11/2021');
+insert into pedidos(cliente, importe_total, fecha) values(2, 15.30, '24/11/2021');
 
 insert into detalle_pedido(producto,cantidad,pedido) values(1,'1 1/2',1);
 insert into detalle_pedido(producto,cantidad,pedido) values(2,'1',1);
