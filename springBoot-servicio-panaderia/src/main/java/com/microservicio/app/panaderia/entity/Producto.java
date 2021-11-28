b21e1df8eb87d60aef367fef441ff8320d935936
walter palomino <jimmypalomino93@gmail.com>
2021-11-27 21:32:43 -0300

    Se agrego atributos a la clase Producto

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/entity/Producto.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/entity/Producto.java
@@ -1,12 +1,6 @@
 package com.microservicio.app.panaderia.entity;
 
-
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,11 +12,16 @@ import java.math.BigDecimal;
 @Entity
 @Table(name="producto")
 public class Producto {
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private BigDecimal precio;
+	@Column(name = "stock_minimo")
+	private int stockMinimo;
+	@Column(name = "stock_maximo")
+	private int stockMaximo;
+	@Column(name = "stock_actual")
+	private int stockActual;
 	}
