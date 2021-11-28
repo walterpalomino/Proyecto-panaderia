a0cf2fd66d404a90eaaf7502fda546f4f154e23e
walter palomino <jimmypalomino93@gmail.com>
2021-11-27 22:46:53 -0300

    Se creo la clase que implementa los metodos del service producto y se construyo logica para buscar todos los productos

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/servicioImpl/ProductoServiceImpl.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/servicioImpl/ProductoServiceImpl.java
@@ -1,2 +1,54 @@
-package com.microservicio.app.panaderia.servicio.servicioImpl;public class ProductoServiceImpl {
+package com.microservicio.app.panaderia.servicio.servicioImpl;
+
+import com.microservicio.app.panaderia.dto.ProductoCrearDto;
+import com.microservicio.app.panaderia.dto.ProductoDto;
+import com.microservicio.app.panaderia.repository.ProductoRepository;
+import com.microservicio.app.panaderia.servicio.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public List<ProductoDto> listaProductos() {
+
+        return productoRepository.findAll()
+                .stream()
+                .map(productoDto -> ProductoDto.builder()
+                        .id(productoDto.getId())
+                        .nombre(productoDto.getNombre())
+                        .precio(productoDto.getPrecio())
+                        .stockMinimo(productoDto.getStockMinimo())
+                        .stockMaximo(productoDto.getStockMaximo())
+                        .stockActual(productoDto.getStockActual())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public ProductoDto addProducto(ProductoCrearDto productoCrearDto) {
+        return null;
+    }
+
+    @Override
+    public ProductoDto buscarProductoId(long id) {
+        return null;
+    }
+
+    @Override
+    public ProductoDto actualizarProducto(long id, ProductoCrearDto productoCrearDto) {
+        return null;
+    }
+
+    @Override
+    public void eliminarProducto(long id) {
+
+    }
 }
