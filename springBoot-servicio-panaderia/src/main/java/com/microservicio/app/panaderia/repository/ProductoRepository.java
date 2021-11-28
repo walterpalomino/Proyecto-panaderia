d51da5b7542c487a5cd5c13a70482e5bfcc35a7a
walter palomino <jimmypalomino93@gmail.com>
2021-11-27 22:41:44 -0300

    Se agredo el repositorio para el producto

---
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.microservicio.app.panaderia.repository;
+
+import com.microservicio.app.panaderia.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
