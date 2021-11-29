0792006e14a3e352896b5006222405f806a36bfe
walter palomino <jimmypalomino93@gmail.com>
2021-11-29 01:23:04 -0300

    Se creo la clase DTO de la clase detalle pedido

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/dto/DetallePedidoDto.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/dto/DetallePedidoDto.java
@@ -1,2 +1,32 @@
-package com.microservicio.app.panaderia.dto;public class DetallePedidoDto {
+package com.microservicio.app.panaderia.dto;
+
+import com.microservicio.app.panaderia.entity.DetallePedido;
+import com.microservicio.app.panaderia.entity.Pedido;
+import com.microservicio.app.panaderia.entity.Producto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+public class DetallePedidoDto {
+
+    private Long id;
+    private Producto producto;
+    private String cantidad;
+    private Pedido pedido;
+    private double precioUnitario;
+    private double subTotal;
+
+    public DetallePedidoDto(DetallePedido detallePedido){
+
+        this.id = detallePedido.getId();
+        this.producto = detallePedido.getProducto();
+        this.cantidad = detallePedido.getCantidad();
+        this.pedido = detallePedido.getPedido();
+        this.precioUnitario = detallePedido.getPrecioUnitario();
+        this.subTotal = detallePedido.getSubTotal();
+    }
 }
