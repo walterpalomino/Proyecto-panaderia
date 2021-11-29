a4b07f2a707e1e3c2b2778e566e7fbbd16737ffe
walter palomino <jimmypalomino93@gmail.com>
2021-11-28 21:44:23 -0300

    Se el recurso que buscar un producto por el id

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/contrioller/ProductoController.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/contrioller/ProductoController.java
@@ -4,6 +4,8 @@ import com.microservicio.app.panaderia.dto.ProductoDto;
 import com.microservicio.app.panaderia.servicio.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,16 @@ public class ProductoController {
     @Autowired
     private ProductoService productoService;
 
+    @GetMapping("/listado-producto")
     public ResponseEntity<List<ProductoDto>> listaProducto(){
 
         return ResponseEntity.ok(productoService.listaProductos());
+    }
+
+    @GetMapping("buscar-producto/{id}")
+    public ResponseEntity<ProductoDto> buscarProductoId(@PathVariable long id){
+
+        return ResponseEntity.ok(productoService.buscarProductoId(id));
     }
 
 }
