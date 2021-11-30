d1ed6eefeba7bb980c29ac10f225c622a3413334
walter palomino <jimmypalomino93@gmail.com>
2021-11-30 01:15:36 -0300

    Se implemento el servicio que busca el detalle pedido por el id

--- springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/servicioImpl/DetallePedidoServiceImpl.java
+++ springBoot-servicio-panaderia/src/main/java/com/microservicio/app/panaderia/servicio/servicioImpl/DetallePedidoServiceImpl.java
@@ -2,15 +2,22 @@ package com.microservicio.app.panaderia.servicio.servicioImpl;
 
 import com.microservicio.app.panaderia.dto.DetallePedidoCrearDto;
 import com.microservicio.app.panaderia.dto.DetallePedidoDto;
+import com.microservicio.app.panaderia.repository.DetallePedidoRepository;
 import com.microservicio.app.panaderia.servicio.DetallePedidoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @Slf4j
 public class DetallePedidoServiceImpl implements DetallePedidoService {
+
+    @Autowired
+    private DetallePedidoRepository detallePedidoRepository;
+
     @Override
     public List<DetallePedidoDto> listadoDetallePedido() {
         return null;
@@ -18,7 +25,16 @@ public class DetallePedidoServiceImpl implements DetallePedidoService {
 
     @Override
     public DetallePedidoDto buscarDetallePedido(long id) {
-        return null;
+        return detallePedidoRepository.findById(id)
+                .map(detallePedido -> DetallePedidoDto.builder()
+                        .id(detallePedido.getId())
+                        .pedido(detallePedido.getPedido())
+                        .cantidad(detallePedido.getCantidad())
+                        .producto(detallePedido.getProducto())
+                        .precioUnitario(detallePedido.getPrecioUnitario())
+                        .subTotal(detallePedido.getSubTotal())
+                        .build())
+                .orElseThrow(()-> new NoSuchElementException("El id " + id + " del detalle pedido no existe"));
     }
 
     @Override
