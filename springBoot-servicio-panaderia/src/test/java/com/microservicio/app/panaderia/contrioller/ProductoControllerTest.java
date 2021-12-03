34ad7b97be682cb85477d76350c3b681314e4e1a
walter palomino <jimmypalomino93@gmail.com>
2021-12-03 00:41:12 -0300

    Se creo la clase test del controlador producto

---
+++ springBoot-servicio-panaderia/src/test/java/com/microservicio/app/panaderia/contrioller/ProductoControllerTest.java
@@ -0,0 +1,33 @@
+package com.microservicio.app.panaderia.contrioller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listaProducto() {
+    }
+
+    @Test
+    void buscarProductoId() {
+    }
+
+    @Test
+    void actualizarProducto() {
+    }
+
+    @Test
+    void eliminarProducto() {
+    }
+
+    @Test
+    void nuevoProducto() {
+    }
+}
