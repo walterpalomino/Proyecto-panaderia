7f3fb3837db1071735f0d42b8b3366d8e7e00182
walter palomino <jimmypalomino93@gmail.com>
2021-12-03 00:39:44 -0300

    Se creo la clase test del detalle pedido

---
+++ springBoot-servicio-panaderia/src/test/java/com/microservicio/app/panaderia/servicio/servicioImpl/DetallePedidoServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.microservicio.app.panaderia.servicio.servicioImpl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DetallePedidoServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listadoDetallePedido() {
+    }
+
+    @Test
+    void buscarDetallePedido() {
+    }
+
+    @Test
+    void actualizarDetallePedido() {
+    }
+
+    @Test
+    void nuevoDetallePedido() {
+    }
+
+    @Test
+    void eliminarDetallePedido() {
+    }
+}
